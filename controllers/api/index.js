4357d02addf031de53fc5fe203548284c42499e4
TopGek99 <a1767142@student.adelaide.edu.au>
2021-03-23 19:17:51 +1030

    wrote parts of homeRoutes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const employeeRoutes = require('./employeeRoutes');
-const managerRoutes = require('./managerRoutes')
-const taskRoutes = require('./tasktRoutes')
+const managerRoutes = require('./managerRoutes');
+const taskRoutes = require('./taskRoutes');
 
 router.use('./employees', employeeRoutes);
-router.use('./managers', managerRoutes);
-router.use('./tasks', taskRoutes);
+// router.use('./managers', managerRoutes);
+// router.use('./tasks', taskRoutes);
 
 module.exports = router;
