23e98a7a374a2a7ae0c3bd1ac00eda91c17bded2
TopGek99 <a1767142@student.adelaide.edu.au>
2021-03-24 13:36:53 +1030

    updated routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,9 +10,31 @@ router.get('/', (req, res) => {
 	}
 });
 
-router.get('/employee/:id', async (req, res) => {
+router.get('/manager', async (req, res) => {
 	try {
-		const employeeData = await Employee.findByPk(req.params.id, {
+		const managerData = await Manager.findByPk(req.session.manager_id, {
+			include: [
+				{
+					model: Task,
+					attributes: ['title', 'time', 'length'],
+				},
+			],
+		});
+
+		const manager = managerData.get({ plain: true });
+
+		res.render('manager', {
+			...manager,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/employee', async (req, res) => {
+	try {
+		const employeeData = await Employee.findByPk(req.session.employee_id, {
 			include: [
 				{
 					model: Task,
@@ -30,36 +52,6 @@ router.get('/employee/:id', async (req, res) => {
 	} catch (err) {
 		res.status(500).json(err);
 	}
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/dashboard', withAuth, async (req, res) => {
-	try {
-		// Find the logged in user based on the session ID
-		const userData = await User.findByPk(req.session.user_id, {
-			attributes: { exclude: ['password'] },
-			include: [{ model: Post }],
-		});
-
-		const user = userData.get({ plain: true });
-
-		res.render('dashboard', {
-			...user,
-			logged_in: true,
-		});
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
-
-router.get('/login', (req, res) => {
-	// If the user is already logged in, redirect the request to another route
-	if (req.session.logged_in) {
-		res.redirect('/dashboard');
-		return;
-	}
-
-	res.render('login');
 });
 
 module.exports = router;
