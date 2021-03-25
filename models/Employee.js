492499f61bb9c997cc25ec5d104e4b47e285de5b
TopGek99 <arowe890@gmail.com>
2021-03-25 10:32:18 +1030

    updated some routes

--- models/Employee.js
+++ models/Employee.js
@@ -1,76 +1,76 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
 
 class Employee extends Model {
-	checkPassword(loginPw) {
-		return bcrypt.compareSync(loginPw, this.password);
-	}
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
 Employee.init(
-	{
-		id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true,
-		},
-		first_name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		last_name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		email: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			unique: true,
-			validate: {
-				isEmail: true,
-			},
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				len: [8],
-			},
-		},
-
-		unavailability: {
-			type: DataTypes.DATE,
-		},
-		manager_id: {
-			type: DataTypes.INTEGER,
-			references: {
-				model: 'manager',
-				id: 'id',
-			},
-		},
-	},
-	{
-		hooks: {
-			beforeCreate: async (newUserData) => {
-				newUserData.password = await bcrypt.hash(newUserData.password, 10);
-				return newUserData;
-			},
-			beforeUpdate: async (updatedUserData) => {
-				updatedUserData.password = await bcrypt.hash(
-					updatedUserData.password,
-					10
-				);
-				return updatedUserData;
-			},
-		},
-		sequelize,
-		timestamps: false,
-		freezeTableName: true,
-		underscored: true,
-		modelName: 'employee',
-	}
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    unavailability: {
+      type: DataTypes.DATE,
+      defaultValue: [],
+    },
+    manager_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "manager",
+        id: "id",
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "employee",
+  }
 );
 
 module.exports = Employee;
