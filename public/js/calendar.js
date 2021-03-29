3b13d4dcdea15e2125c8afe3bf6f2cb677b96359
Alex Rowe <arowe890@gmail.com>
2021-03-29 19:14:29 +1030

    Revert "All changes from bulma-calendar integration."

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,21 +1,6 @@
 // Initialize all input of date type.
-const options = {
-	color: 'primary',
-	type: 'datetime',
-	startDate:'03-29-2021',
-	endDate:'04-05-2021',
-	dateFormat: 'DD-MM-YYYY',
-	startTime: '09:00',
-	endTime: '17:00',
-	timeFormat: 'HH:mm A',
-	displayMode: 'inline',
-	lang: 'en',
-	isRange: 'true',
-	allowSameDayRange: 'true',
-	toggleOnInputClick: 'true',
-}
-const calendars = bulmaCalendar.attach('[type = "datetime"]', options);
-console.log(calendars);
+const calendars = bulmaCalendar.attach('[type="date"]',{});
+console.log(calendars)
 // Loop on each calendar initialized
 calendars.forEach(calendar => {
 	// Add listener to select event
@@ -25,12 +10,10 @@ calendars.forEach(calendar => {
 });
 
 // To access to bulmaCalendar instance of an element
-const element = document.querySelector('#calendar');
+const element = document.querySelector('#my-element');
 if (element) {
 	// bulmaCalendar instance is available as element.bulmaCalendar
-	element.bulmaCalendar.on('select', (datepicker, timepicker) => {
-		console.log(datepicker.data.value(startDate, endDate));
-		console.log(timepicker.data.value(startTime, endTime));
+	element.bulmaCalendar.on('select', datepicker => {
+		console.log(datepicker.data.value());
 	});
-	
 }
